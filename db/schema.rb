d7322d588b6025e1ad06a458ecfc4b9739a89e67
flaviaza <flavia.ibanez@gmail.com>
2014-09-25 08:36:38 -0300

    mend

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140923180119) do
+ActiveRecord::Schema.define(:version => 20140924125144) do
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0, :null => false
+    t.integer  "attempts",   :default => 0, :null => false
+    t.text     "handler",                   :null => false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "documents", :force => true do |t|
     t.string   "name"
@@ -21,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20140923180119) do
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
     t.integer  "user_id"
+    t.string   "status"
   end
 
   create_table "users", :force => true do |t|
